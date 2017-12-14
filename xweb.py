28501bce0e987f9f109ab3d2dec0f92f75186968
Kham <spacekhammy@gmail.com>
2017-12-13 20:08:50 -0500

    back

--- xweb.py
+++ xweb.py
@@ -79,9 +79,6 @@ def hello():
             return render_template("bal.html", balance=new_bal)
         elif plus_minus == "expense":
             new_bal = bal.minus(obj)
-            for ba in bal.lst:
-                print(ba.amt)
-            print(new_bal)
             return render_template("bal.html", balance=new_bal)
     else:
         return render_template("bal.html")
